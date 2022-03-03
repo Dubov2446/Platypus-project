c1614912aa45a0ff2e037ddbf8d68629282aaea3
ArtemSuprun1 <100882414+ArtemSuprun1@users.noreply.github.com>
2022-03-03 15:04:26 -0800

    Implementation file of Platypus class

---
+++ platypus.cpp
@@ -0,0 +1,10 @@
+/*
+  
+  File Name:  platypus.cpp
+  Authors:    Josue lopez, Artem Suprun
+  Date:       03/03/2022
+  Summary:    Implementation of the Platypus class
+  
+*/
+
+#include "platypus.h"
